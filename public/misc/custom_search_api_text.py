f594428653e55ac55c4610b47afb9a50641c5217
Lukas Geiger <geiger.ljo@gmail.com>
2024-12-28 11:03:25 -0500

    removed security risk

--- public/misc/custom_search_api_text.py
+++ public/misc/custom_search_api_text.py
@@ -22,7 +22,7 @@ Command-line application that does a search.
 
 __author__ = "jcgregorio@google.com (Joe Gregorio)"
 
-import pprint
+import pprint, os
 
 from googleapiclient.discovery import build
 
@@ -31,9 +31,9 @@ def main():
     # Build a service object for interacting with the API. Visit
     # the Google APIs Console <http://code.google.com/apis/console>
     # to get an API key for your own application.
-    service = build(
-        "customsearch", "v1", developerKey="AIzaSyDpkqHySSO0ikmGnanCiFH3KXo1db_XQAI"
-    )
+    service = build("customsearch",
+                    "v1",
+                    developerKey=os.environ.get("CUSTOM_SEARCH_API_KEY"))
 
     res = (
         service.cse()
