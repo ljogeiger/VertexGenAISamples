beeb81cd4989730d4dec613a04b29acc5ed891df
Lukas Geiger <geiger.ljo@gmail.com>
2025-05-08 13:12:07 -0400

    Modified thinking to include_thoughts=True config

--- public/gemini/gemini-thinking.py
+++ public/gemini/gemini-thinking.py
@@ -1,16 +1,58 @@
 from google import genai
 from google.genai import types
+import base64
 
-client = genai.Client(vertexai=True,
-                      project="sandbox-aiml",
-                      location="global")
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      project="sandbox-aiml",
+      location="global",
+  )
 
-response = client.models.generate_content(
-    model="gemini-2.5-pro-preview-03-25",
-    contents="solve x^2 + 4x + 4 = 0",
-    config=types.GenerateContentConfig(system_instruction=["Provide step by step guidance on your thought process."]),
-)
-print(response.text)
+
+  model = "gemini-2.5-flash-preview-04-17"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text(text="""solve x^2 + 4x + 4 = 0""")
+      ]
+    ),
+  ]
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 0.95,
+    seed = 0,
+    max_output_tokens = 8192,
+    response_modalities = ["TEXT"],
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+    system_instruction=[types.Part.from_text(text="""Provide step by step guidance on your thought process""")],
+    thinking_config=types.ThinkingConfig(
+        include_thoughts=True,
+    )
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+    print(chunk)
+
+generate()
 
 """ EXAMPLE RESPONSE
 Okay, let's solve the quadratic equation x² + 4x + 4 = 0 step by step.
